38c584062c708368774c67abc9c54adf03e57760
Neha Kumari <bodhisattva@Prashanths-iMac-2.local>
2014-07-08 14:27:01 -0700

    Fix faulty prime test. 9 is not prime

--- spec/lib/prime_generator_spec.rb
+++ spec/lib/prime_generator_spec.rb
@@ -4,7 +4,7 @@ describe PrimeGenerator do
   describe "generate" do
     it "returns an array of primes" do
       pending("Implement prime number generators")
-      PrimeGenerator.new(10, SieveOfAtkin.new).generate.to eq [2,3,5,7,9,11,13,17,19,23]
+      PrimeGenerator.new(10, SieveOfAtkin.new).generate.to eq [2,3,5,7,11,13,17,19,23,29]
     end
   end
 end
