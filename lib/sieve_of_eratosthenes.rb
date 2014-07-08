77d6178df526ff71feb62220e02344084bdcadf1
Neha Kumari <bodhisattva@Prashanths-iMac-2.local>
2014-07-08 14:26:08 -0700

    Implement a basic sieve of eratosthenes

--- lib/sieve_of_eratosthenes.rb
+++ lib/sieve_of_eratosthenes.rb
@@ -1,5 +1,23 @@
 class SieveOfEratosthenes
-  def generate(count)
+  def initialize(count)
+    @count = count
+  end
 
+  def generate
+    return [] if @count < 1
+    primes = [2]
+    current = 3
+    while(primes.length < @count)
+      primes << current if is_prime?(current, primes)
+      current += 2
+    end
+    primes
+  end
+
+  private
+  def is_prime?(number, primes)
+    prime_limit = Math.sqrt(number).floor
+    potential_prime_factors = primes.select{|p| p <= prime_limit}
+    !potential_prime_factors.any?{|p| number % p == 0}
   end
 end
--- spec/lib/sieve_of_eratosthenes_spec.rb
+++ spec/lib/sieve_of_eratosthenes_spec.rb
@@ -2,10 +2,26 @@ require_relative '../../lib/sieve_of_eratosthenes.rb'
 
 describe SieveOfEratosthenes do
   describe "generate" do
-    it "returns required number of primes"
+    it "returns an empty array when number of primes N is 0" do
+      sieve = SieveOfEratosthenes.new(0)
+      expect(sieve.generate).to be_empty
+    end
 
+    it "returns 2 when required number of primes N is 1" do
+      sieve = SieveOfEratosthenes.new(1)
+      expect(sieve.generate).to match_array [2]
+    end
 
-    it "returns only prime numbers"
+    it "returns 10 prime numbers when required number of primes N is 10" do
+      sieve = SieveOfEratosthenes.new(10)
+      expect(sieve.generate).to match_array [2,3,5,7,11,13,17,19,23, 29]
+    end
 
+    it "generates only prime number" do
+      sieve = SieveOfEratosthenes.new(1000)
+      primes = sieve.generate
+      random_prime = primes[rand(primes.length - 1)]
+      expect(primes.any?{|p| random_prime % p == 0 if random_prime != p}).to eq false
+    end
   end
 end
